d3de3970cbb4ca2eadd512e54833d4f173abfbec
unknown <pakphil7@gmail.com>
2020-06-16 15:26:25 -0700

    added api routes

--- exerciseModel.js
+++ exerciseModel.js
@@ -3,24 +3,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
-    type: {
-        type: String
+    day: {
+        type: Number,
+        default: Date.now
     },
-    name: {
-        type: String
-    },
-    duration: {
-        type: Number
-    },
-    weight: {
-        type: Number
-    },
-    reps: {
-        type: Number
-    },
-    set: {
-        type: Number
-    }
+
+    exercises: 
+    [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Select a workout type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter the name of the workout"
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
 });
 
 const Exercise = mongoose.model("Exercise", ExerciseSchema);
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 const Exercise = require("./exerciseModel.js");
+const { Router } = require("express");
 const app = express();
 
 app.use(logger("dev"));
@@ -27,16 +28,53 @@ app.get("/exercise", (req, res) => {
 
 app.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/stats.html"));
-})
-// app.post("/submit", ({ body }, res) => {
-//   User.create(body)
-//     .then(exercise => {
-//       res.json(exercise);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
+});
+
+app.post("/api/workouts", ({ body }, res) => {
+  Exercise.create({ $push: {exercises: body}})
+    .then(exercise => {
+      res.json(exercise);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/api/workouts", (req, res) => {
+  Exercise.find({})
+    .sort({ date: -1 })
+    .then(exercise => {
+      res.json(exercise);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  Exercise.find({})
+    .then(exercise => {
+      res.json(exercise);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+app.put("/api/workouts/:id", ({ params, body}, res) => {
+  Exercise.findByIdAndUpdate(
+      { _id: params.id }, 
+      { $set: {exercises: body}}, 
+      {new: true, runvalidators: true})
+      .then(exercise => {
+          if(!exercise){
+              res.status(400).json({message: "no workout found with this id"});
+              return;
+          }
+          res.json(exercise);
+   
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
